6273ea8e41e300633a1d857a3eca276a0ae70f79
Homa Wong <homawong@gmail.com>
2016-07-12 00:43:44 -0700

    Use relative path

--- src/resolve.spec.ts
+++ src/resolve.spec.ts
@@ -1,10 +1,9 @@
-import { join } from 'path';
 import fixture from 'blue-tape-fixture';
 
 import { resolve } from './index';
 
 
-const FIXTURES_DIR = join(__dirname, '_fixture_/cases');
+const FIXTURES_DIR = './src/_fixture_/cases';
 const ftest = fixture(FIXTURES_DIR);
 ftest('jspm resolve()', '0.17-custom', (t, cwd) => {
   return resolve({ cwd });
