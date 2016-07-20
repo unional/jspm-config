c11b9ae98c439cc66506a1485b456d64fb9de237
Homa Wong <homawong@gmail.com>
2016-07-19 17:19:58 -0700

    Adding tests for readProjectConfig (#5)

--- src/readProjectConfig.spec.ts
+++ src/readProjectConfig.spec.ts
@@ -5,8 +5,8 @@ import { readProjectConfig } from './index';
 
 const ftest = fixture(ava, '../fixtures/cases');
 
-ftest('readProjectConfig()', '0.17-custom', (t, cwd) => {
-  return readProjectConfig({ cwd })
+ftest('readProjectConfig()', '0.17-custom', (t, casePath) => {
+  return readProjectConfig({ cwd: casePath })
     .then((projectInfo) => {
       t.deepEqual(projectInfo, {
         jspmPackageJson: {
@@ -27,9 +27,40 @@ ftest('readProjectConfig()', '0.17-custom', (t, cwd) => {
           jspm: { packageConfigPaths: [], map: {}, packages: {} },
           browser: { baseURL: '/', paths: { 'app/': 'src/' } },
           dev: {},
-          node: { paths: { 'app/': 'src/'} }
+          node: { paths: { 'app/': 'src/' } }
         },
         dependenciesJson: {}
       }, 'should work with full custom configuration');
+    });
+});
+
+ftest('readyProjectConfig()', 'empty', (t, casePath) => {
+  return readProjectConfig({ cwd: casePath })
+    .then((projectInfo) => {
+      t.deepEqual(projectInfo, undefined, 'should return undefined when the folder is empty');
+    });
+});
+
+ftest('readProjectConfig()', 'non-jspm-empty', (t, casePath) => {
+  return readProjectConfig({ cwd: casePath })
+    .then((projectInfo) => {
+      t.deepEqual(projectInfo, {
+        jspmPackageJson: {
+          directories: {
+            baseURL: '.',
+            packages: 'jspm_packages'
+          },
+          configFiles: {
+            jspm: 'jspm.config.js',
+            'jspm:browser': 'jspm.browser.js',
+            'jspm:dev': 'jspm.dev.js',
+            'jspm:node': 'jspm.node.js'
+          },
+          name: 'non-jspm',
+          main: 'index.js'
+        },
+        jspmConfigs: {},
+        dependenciesJson: undefined
+      }, 'should work with non-jspm empty library');
     });
 });
