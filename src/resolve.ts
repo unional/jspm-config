247a76f0002da6387a49f473c7694f64c461471b
Homa Wong <homawong@gmail.com>
2016-07-23 16:19:23 -0700

    Fix lint

--- src/resolve.ts
+++ src/resolve.ts
@@ -7,7 +7,7 @@ import { readJspmPackageJson, readJspmConfigs } from './readProjectConfig'
 export function resolveAll(options: Options): Promise<DependencyTree> {
   return readJspmPackageJson(options)
     .then(pjson => {
-      return readJspmConfigs(pjson, options);
+      return readJspmConfigs(pjson, options)
     })
     .then(configs => {
       const dependencyInfo = getDependencyInfo(configs)
@@ -21,7 +21,7 @@ export function resolveAll(options: Options): Promise<DependencyTree> {
 export function resolve(moduleName: string, options: Options): Promise<DependencyTree> {
   return readJspmPackageJson(options)
     .then(pjson => {
-      return readJspmConfigs(pjson, options);
+      return readJspmConfigs(pjson, options)
     })
     .then(configs => {
       const dependencyInfo = getDependencyInfo(configs)
