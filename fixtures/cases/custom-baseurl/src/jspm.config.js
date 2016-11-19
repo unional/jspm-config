1f50bdb4bf94b4e08d60a95f3b12ded6f2af3ba4
Homa Wong <homawong@gmail.com>
2016-11-18 19:39:53 -0800

    Fix custom-base-url case
    Where baseURL is specified but packages is not.

---
+++ fixtures/cases/custom-baseurl/src/jspm.config.js
@@ -0,0 +1,21 @@
+SystemJS.config({
+  paths: {
+    "github:": "jspm_packages/github/",
+    "npm:": "jspm_packages/npm/",
+    "app/": "src/"
+  },
+  browserConfig: {
+    "baseURL": "."
+  }
+})
+
+SystemJS.config({
+  packageConfigPaths: [
+    "github:*/*.json",
+    "npm:@*/*.json",
+    "npm:*.json"
+  ],
+  map: {
+    "make-error": "npm:make-error@1.2.1",
+  }
+})
